c14c46eedfaf865e52b105e1bf2fcd8c2d78a0bb
Cameron Soregaroli <csoregaroli@gmail.com>
2023-01-17 21:10:11 -0500

    set up addNewLaunch function

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map()
 
+let latestFlightNumber = 100
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
@@ -13,6 +15,24 @@ const launch = {
 
 launches.set(launch.flightNumber, launch)
 
+function getAllLaunches() {
+  return Array.from(launches.values())
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customers: ['ZTM', 'NASA'],
+      upcoming: true,
+      success: true,
+    })
+  )
+}
+
 module.exports = {
-  launches,
+  getAllLaunches,
+  addNewLaunch,
 }
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -39,7 +39,11 @@ function loadPlanetsData() {
   })
 }
 
+function getAllPlanets() {
+  return habitablePlanets
+}
+
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets,
+  getAllPlanets,
 }
