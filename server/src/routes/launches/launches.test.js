638a7d9222689c85e1fe93628d8decc609b719dd
Cameron Soregaroli <csoregaroli@gmail.com>
2023-01-23 07:30:37 -0500

    created error case api tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -24,6 +24,13 @@ describe('Test POST /launches', () => {
     target: 'Kepler-186 f',
   }
 
+  const launchDataInvalidDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+    launchDate: 'zoot',
+  }
+
   test('It should respond with 201 created', async () => {
     const response = await request(app)
       .post('/launches')
@@ -38,6 +45,27 @@ describe('Test POST /launches', () => {
     expect(response.body).toMatchObject(launchDataWithoutDate)
   })
 
-  test('It should catch missing required properties', () => {})
-  test('It should catch invalid dates', () => {})
+  test('It should catch missing required properties', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    expect(response.body).toStrictEqual({
+      error: 'Missing required launch property',
+    })
+  })
+
+  test('It should catch invalid dates', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataInvalidDate)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    expect(response.body).toStrictEqual({
+      error: 'Invalid launch date',
+    })
+  })
 })
