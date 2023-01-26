320497180c6ded7eb00ea1f9af6e636952e8b923
Cameron Soregaroli <csoregaroli@gmail.com>
2023-01-26 08:58:53 -0500

    set up abort launch function with mongo data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,8 +20,10 @@ const launch = {
 
 saveLaunch(launch)
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId)
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId,
+  })
 }
 
 async function getLatestFlightNumber() {
@@ -48,7 +50,7 @@ async function saveLaunch(launch) {
     throw new Error('No matching planet was found')
   }
 
-  await launchesDatabase.updateOne(
+  await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
@@ -72,11 +74,18 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch)
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId)
-  aborted.upcoming = false
-  aborted.success = false
-  return aborted
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  )
+
+  return aborted.modifiedCount === 1
 }
 
 module.exports = {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -34,17 +34,23 @@ async function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch)
 }
 
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
   const launchId = Number(req.params.id)
+  const existsLaunch = await existsLaunchWithId(launchId)
 
-  if (!existsLaunchWithId(launchId)) {
+  if (!existsLaunch) {
     return res.status(404).json({
       error: 'Launch not found',
     })
   }
 
-  const aborted = abortLaunchById(launchId)
-  return res.status(200).json(aborted)
+  const aborted = await abortLaunchById(launchId)
+
+  if (!aborted) {
+    return res.status(400).json({ error: 'Launch not aborted' })
+  }
+
+  return res.status(200).json({ ok: true })
 }
 
 module.exports = { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch }
