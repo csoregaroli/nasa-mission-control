25cbb6708b4a7eb4142f7a6c4ed12d54891857bf
Cameron Soregaroli <csoregaroli@gmail.com>
2023-01-29 23:32:03 -0500

    added sorting to getLaunches enpoint

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require('./planets.mongo')
 
 const DEFAULT_FLIGHT_NUMBER = 100
 
-const launch = {
-  flightNumber: 100, // flight_number
-  mission: 'Kepler Exploration X', // name
-  rocket: 'Explorer IS1', // rocket.name
-  launchDate: new Date('December 27, 2030'), // date_local
-  target: 'Kepler-442 b', // N/A
-  customers: ['NASA', 'ZTM'], // payloads.customers for each payload
-  upcoming: true, // upcoming
-  success: true, // success
-}
-
-saveLaunch(launch)
-
 // SpaceX API functions
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
 
@@ -102,6 +89,7 @@ async function getLatestFlightNumber() {
 async function getAllLaunches(skip, limit) {
   return await launchesDatabase
     .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit)
 }
